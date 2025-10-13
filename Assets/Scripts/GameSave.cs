c7b0db3448d6146d75248e1888e021525a45a2a4
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2025-10-13 15:04:39 +0800

    Update
    添加预制体和BUG警告材质

---
+++ Assets/Scripts/GameSave.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//管理游戏存档的脚本
+public class GameSave : MonoBehaviour
+{
+    
+}
+
+//存档文件
+public class SaveData
+{
+    
+}
