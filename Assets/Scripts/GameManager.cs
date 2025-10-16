fc34c38229f4791ed1088b92cf5045629bcbbdcb
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2025-10-16 17:29:30 +0800

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,14 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Animator AlertAnim;
 
+    private void Start()
+    {
+        //开始时，生成角色
+    }
+
     //触发BUG时播放警告动画
     public void BugAlert()
     {
-        
+        AlertAnim.Play("BUG触发效果");
     }
 }
