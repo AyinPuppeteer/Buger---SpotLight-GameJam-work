27cc8eea4a98a91df32283f65a46f56fedecfbb2
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2025-10-24 22:15:26 +0800

    Update

--- Assets/Scripts/GameBlocks/HideField.cs
+++ Assets/Scripts/GameBlocks/HideField.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//隐藏区域（草丛）的脚本
+//隐藏区域（柜子）的脚本
 public class HideField : MonoBehaviour, I_Interacts
 {
     public void TakeInteract()
     {
-        if (BaseMovement.Instance != null && BaseMovement.Instance.IsDetectable_)
+        if (BaseMovement.Instance != null) 
         {
-            BaseMovement.Instance.SetDetectable(false);
-        }
-        else if (BaseMovement.Instance != null && !BaseMovement.Instance.IsDetectable_)
-        {
-            BaseMovement.Instance.SetDetectable(true);
+            if (BaseMovement.Instance.IsDetectable_)
+            {
+                BaseMovement.Instance.SetDetectable(false);
+            }
+            else if (!BaseMovement.Instance.IsDetectable_)
+            {
+                BaseMovement.Instance.SetDetectable(true);
+            }
         }
     }
 }
